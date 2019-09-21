3fbfcf1b7a804c1a32c4b96b17e2dedd1fc3ba5d
BalajiVoore <balu.voore432@gmail.com>
2019-09-21 12:42:10 +0530

    updated Summay name

--- jobs/models.py
+++ jobs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class job(models.Model):
     imageNew= models.ImageField(upload_to='images')
-    summary= models.CharField(max_length=200)
+    summaryNew= models.CharField(max_length=200)
 
 
 
