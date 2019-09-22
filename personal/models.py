8c10c59725b4a274ed63f68853da9d96877de832
BalajiVoore <balu.voore432@gmail.com>
2019-09-22 23:49:06 +0530

    Modified some URLS

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class personal(models.Model):
+   goal=models.CharField(max_length=50)
+   conditions=models.CharField(max_length=100)
+   pub_date=models.DateTimeField()
+   def condi(self):
+        return self.conditions[:50]
+   def dating(self):
+       return self.pub_date.strftime('%B %e %Y')
+   def __str__(self):
+       return self.goal
+
+
--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import personal
 # Create your views here.
+def myself(request):
+    per=personal.objects
+    return render(request,'personal.html',{'per':per})
